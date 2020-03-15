ba6079df32f813152c08de7d786b8a1f674aa60d
Sofia Franek <sofiafranek@icloud.com>
2020-03-15 17:37:21 +0000

    working tree

--- client/src/Views/Cards/index.jsx
+++ client/src/Views/Cards/index.jsx
@@ -4,12 +4,24 @@ import Card from './../../Components/Card';
 import Layout from '../../Components/Layout';
 import { cards } from './../../Services/card';
 
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+
 class Cards extends Component {
   constructor() {
     super();
     this.state = {
       cards: []
     };
+  }
+
+  addingCard() {
+    console.log('add card');
+  }
+
+  refresh() {
+    window.location.reload();
+    console.log('refresh');
   }
 
   componentDidMount() {
@@ -31,13 +43,24 @@ class Cards extends Component {
 
   render() {
     return (
-      <div>
+      <Layout>
+        <h1 className="pb-3">Cards</h1>
+        <div className="action-container">
+          <Link to={`/cards/add-card`} onClick={this.addingCard}>
+            <Button variant="contained" className="primary">
+              <i className="fas fa-plus"></i>
+            </Button>
+          </Link>
+          <Button variant="contained" className="secondary" onClick={this.refresh}>
+            <i className="fas fa-sync-alt"></i>
+          </Button>
+        </div>
         <div className="">
           {this.state.cards.map(card => (
             <Card key={card._id} {...card} />
           ))}
         </div>
-      </div>
+      </Layout>
     );
   }
 }

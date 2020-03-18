c1512151b4a76a81f51e7497bfb463747e245410
RaqueCoelho <57578500+RaqueCoelho@users.noreply.github.com>
2020-03-18 11:56:32 +0000

    commit

--- client/src/Views/createCard/index.jsx
+++ client/src/Views/createCard/index.jsx
@@ -12,7 +12,7 @@ import Layout from '../../Components/Layout';
 import { creatingCard } from './../../Services/card';
 import { userIDAccounts } from './../../Services/account';
 
-import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Breadcrumb from 'greact-bootstrap/Breadcrumb';
 
 class CreateCard extends Component {
   constructor(props) {

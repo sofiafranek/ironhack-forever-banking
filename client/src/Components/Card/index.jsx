5a49b00067ff4cd64f35ba4fe1b9063d931eef8e
Sofia Franek <sofiafranek@icloud.com>
2020-03-23 18:11:56 +0000

    fixed card rotation

--- client/src/Components/Card/index.jsx
+++ client/src/Components/Card/index.jsx
@@ -5,6 +5,7 @@ import './style.scss';
 class Card extends Component {
   flip(event) {
     event.preventDefault();
+    console.log('FLIP');
     let element = event.currentTarget;
     if (element.className === 'credit-card__section') {
       if (element.style.transform === 'rotateY(180deg)') {
@@ -44,51 +45,53 @@ class Card extends Component {
         >
           <i className="fas fa-times"></i>
         </button> */}
-        <section className="credit-card__section relative" onClick={event => this.flip(event)}>
-          <button
-            className={
-              this.props.toggle === true ? 'delete-card-button--hide' : 'delete-card-button--show'
-            }
-            onClick={event => this.deleteACard(event)}
-          >
-            <i className="fas fa-times"></i>
-          </button>
-          <div className="credit-card credit-card__front">
-            <div className="credit-card__top">
-              <div className="logo-card"></div>
-              <h6>{type}</h6>
-            </div>
-            <div className="credit-card__number">{cardNumber}</div>
-
-            <div className="credit-card__info">
-              <div className="credit-card__info_name">
-                <div className="credit-card__info_label">CARDHOLDER'S NAME</div>
-                <p>{userName}</p>
+        <section className="credit-card__section" onClick={event => this.flip(event)}>
+          <section className="relative">
+            <button
+              className={
+                this.props.toggle === true ? 'delete-card-button--hide' : 'delete-card-button--show'
+              }
+              onClick={event => this.deleteACard(event)}
+            >
+              <i className="fas fa-times"></i>
+            </button>
+            <div className="credit-card credit-card__front">
+              <div className="credit-card__top">
+                <div className="logo-card"></div>
+                <h6>{type}</h6>
               </div>
+              <div className="credit-card__number">{cardNumber}</div>
 
-              <div className="credit-card__info_expiry">
-                <div className="credit-card__info_label">VALID UP TO</div>
-                <p>{expiryDate}</p>
-              </div>
-            </div>
-          </div>
+              <div className="credit-card__info">
+                <div className="credit-card__info_name">
+                  <div className="credit-card__info_label">CARDHOLDER'S NAME</div>
+                  <p>{userName}</p>
+                </div>
 
-          <div className="credit-card credit-card__back">
-            <div className="credit-card__top">
-              <div className="logo-card"></div>
-              <h6>{type}</h6>
-            </div>
-
-            <div className="credit-card__block">
-              <span>{CVV}</span>
-            </div>
-
-            <div className="credit-card__info">
-              <div className="credit-card__info_name_back">
-                <p>{ (accountID) ? accountID.accountNumber : creditID.accountNumber}</p>
+                <div className="credit-card__info_expiry">
+                  <div className="credit-card__info_label">VALID UP TO</div>
+                  <p>{expiryDate}</p>
+                </div>
               </div>
             </div>
-          </div>
+
+            <div className="credit-card credit-card__back">
+              <div className="credit-card__top">
+                <div className="logo-card"></div>
+                <h6>{type}</h6>
+              </div>
+
+              <div className="credit-card__block">
+                <span>{CVV}</span>
+              </div>
+
+              <div className="credit-card__info">
+                <div className="credit-card__info_name_back">
+                  <p>{accountID ? accountID.accountNumber : creditID.accountNumber}</p>
+                </div>
+              </div>
+            </div>
+          </section>
         </section>
       </section>
     );
